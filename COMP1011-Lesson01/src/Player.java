1c4c03d13cb87daaeff021b080f17b44ed32d310
BlaineParr <Blaine.parr@hotmail.com>
2015-01-08 16:37:43 -0500

    First release

---
+++ COMP1011-Lesson01/src/Player.java
@@ -0,0 +1,19 @@
+
+public class Player
+{
+	
+	public static void main(String[] args) 
+	{
+		int strength = 10;
+		Person tom;
+		
+		
+		System.out.println("Hello World! " + strength);
+
+		tom = new Person();
+		tom.walks();
+		tom.walks();
+		tom.walks();
+	}
+	
+}
